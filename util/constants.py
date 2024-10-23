7839144313dd89dcb6af2c3f8577f02d360c193a
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-10-23 17:03:31 -0300

    changed url format

--- util/constants.py
+++ util/constants.py
@@ -2,5 +2,5 @@ import os
 
 CHROME_DRIVER_PATH = os.path.join("drivers","chromedriver-win64", "chromedriver.exe")
 GECKO_DRIVER_PATH = os.path.join("drivers","geckodriver","geckodriver.exe")
-LINK_CASA_DOS_DADOS = "https://casadosdados.com.br/solucao/cnpj/gilberto-antonio-santos-de-melo-42928714000104" #Exemplo TODO alterar depois
+LINK_CASA_DOS_DADOS = "https://casadosdados.com.br/solucao/cnpj/padaria-and-confeitaria-rafaella-ltda-34252826000160" #Exemplo TODO alterar depois
 OUTPUT_PATH = os.path.join("util","output_file.txt") #arquivo com o filename do arquivo de saída
